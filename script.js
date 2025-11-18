5a9041deada630d8c89fe5d1adf99534babf8929
Eleanorronaele <eleanorbilqis26@gmail.com>
2025-11-18 20:19:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function addTask() {
+    let task = document.getElementById("task").value;
 
+    if (task === "") {
+        alert("Tulis tugas dulu!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = task;
+
+    document.getElementById("list").appendChild(li);
+
+    document.getElementById("task").value = "";
+}
